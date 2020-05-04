c31b482f0c8c446f6d527db7cac22d5765efb965
CohenOfir <59443707+CohenOfir@users.noreply.github.com>
2020-05-04 22:31:06 +0300

    Final

---
+++ FlightSimulatorApp/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using FlightSimulatorApp.Tools;
+using FlightSimulatorApp.ViewModel;
+using FlightSimulatorApp.Views;
+using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace FlightSimulatorApp
+{
+    public partial class MainWindow : Window
+    {
+        MainWindowViewModel vm;
+        ConnectView connectWin;
+        LocationRect bounds;
+        private bool stopped;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.stopped = false;
+            map.SetView(new Location(31.997, 34.886), 15);
+            this.vm = new MainWindowViewModel(new Model.MainWindowModel());
+            //Property Changed Assignment 
+            this.vm.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e) { };
+            ControlUnit.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e)
+            {
+                var args = e as JoyStickPropertyChanged;
+                if (args != null)
+                {
+                    string property = args.PropertyName;
+                    if (property.Equals("Elevator"))
+                    {
+                        this.vm.Elevator = args.Propertyvalue;
+                    }
+
+                    if (property.Equals("Rudder"))
+                    {
+                        {
+                            this.vm.Rudder = args.Propertyvalue;
+                        }
+                    }
+                }
+            };
+            DataContext = this.vm;
+        }
+        //Server Connect
+        private void Button_Click_Connect(object sender, RoutedEventArgs e)
+        {
+            this.connectWin = new ConnectView();
+            this.stopped = false;
+            connectWin.ShowDialog();
+            try
+            {
+                this.vm.model.connectToServer(connectWin.Ip, connectWin.Port);
+            }
+            catch
+            {
+                AutoClosingMessageBox.Show("Connection Failed\nPlease try again\nClosing Window Please wait", "Error", 2500);
+            }
+        }
+        //Disconnected from Server
+        private void Button_Click_Disconnect(object sender, RoutedEventArgs e)
+        {
+            this.vm.model.disconnectServer();
+            this.vm.model.Connected = false;
+            pin.Location.Latitude = 31.997;
+            pin.Location.Longitude = 34.886;
+            map.SetView(new Location(pin.Location.Latitude, pin.Location.Longitude), 15);
+        }
+        //Plane Location on Map
+        private void pinUpdated(object sender, EventArgs e)
+        {
+            if (pin.Location != null && stopped == false)
+            {
+                this.bounds = map.BoundingRectangle;
+                double mapCenterLat = bounds.Center.Latitude,
+                       mapCenterLon = bounds.Center.Longitude,
+                       pinLat = pin.Location.Latitude,
+                       pinLon = pin.Location.Longitude;
+
+                //Current Frame Violation - plane is outside Map Bounds
+                if ((pinLat > bounds.North || pinLat < bounds.South || pinLon > bounds.East || pinLon < bounds.West) && !stopped)
+                {
+                    map.SetView(new Location(pinLat, pinLon), 12);
+                }
+
+                //Map Bounds Violation (Nort,Sout Poles)
+                if ((pinLat >= 86 || pinLat <= -86))
+                {
+                    AutoClosingMessageBox.Show("Map Bounds Violation\nDisconnecting Server\nClosing Window Please wait", "Error", 2500);
+                    this.vm.model.disconnectServer();
+                    this.stopped = true;
+                    pin.Location.Latitude = 32;
+                    pin.Location.Longitude = 34.88;
+                    map.SetView(new Location(pin.Location.Latitude, pin.Location.Longitude), 8);
+                    this.vm.model.Latitude = "32";
+                    this.vm.model.Longitude = "34.88";
+                    this.vm.model.Connected = false;
+                }
+            }
+        }
+        //Window Closed- X button
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (this.vm.model.Connected)
+            {
+                this.vm.model.disconnectServer();
+            }
+        }
+    }
+}
---
+++ FlightSimulatorApp/Model/MainWindowModel.cs
@@ -0,0 +1,368 @@
+﻿using FlightSimulatorApp.Tools;
+using FlightSimulatorApp.Views;
+using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace FlightSimulatorApp.Model
+{
+    class MainWindowModel : INotifyPropertyChanged
+    {
+        private string throttle, aileron, elevator, rudder, latitude, longitude, airspeed, altitude,
+                       roll, pitch, altimeter, heading, groundSpeed, verticalSpeed;
+        private bool disconnected, connected;
+        private TcpClient tcpClient;
+        private NetworkStream netStream;
+        private Queue<string> queue;
+
+
+        public MainWindowModel()
+        {
+            this.latitude = "31.996";
+            this.longitude = "34.8865";
+            disconnected = false;
+            this.queue = new Queue<string>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+        public void connectToServer(string ip, int port)
+        {
+            connected = true;
+            try
+            {
+                this.tcpClient = new TcpClient();
+                this.tcpClient.Connect(ip, port);
+                this.disconnected = false;
+                this.Connected = true;
+                this.netStream = tcpClient.GetStream();
+
+            }
+            catch 
+            {
+                throw new Exception("not connected");
+            }
+
+            try
+            {
+                this.fly();
+            }
+            catch
+            {
+                throw new Exception("connection interupt");
+            }
+
+
+
+        }
+
+        public void disconnectServer()
+        {
+            disconnected = true;
+            tcpClient.Close();
+            connected = false;
+        }
+        //Write to Server
+        public void write(string text)
+        {
+            this.netStream = this.tcpClient.GetStream();
+            ASCIIEncoding asci = new ASCIIEncoding();
+            byte[] msg = asci.GetBytes(text);
+            netStream.Write(msg, 0, msg.Length);
+        }
+        //Read From Server
+        public string read()
+        {
+            byte[] buffer = new byte[100];
+
+            int k = this.netStream.Read(buffer, 0, 100);
+            string text = "";
+            for (int i = 0; i < k; i++)
+                text += (Convert.ToChar(buffer[i]));
+            return text;
+        }
+        //Server Comunication - read and write
+        public void fly()
+        {
+            
+            //New Thred for server comunication
+                new Thread(delegate ()
+                {
+                    while (!this.disconnected)
+                    {
+                        try
+                        {
+                            //Read from server
+                            this.write("get /instrumentation/airspeed-indicator/indicated-speed-kt\n");
+                            this.Airspeed = this.read();
+                            this.write("get /instrumentation/gps/indicated-altitude-ft\n");
+                            this.Altitude = this.read();
+                            this.write("get /instrumentation/attitude-indicator/internal-roll-deg\n");
+                            this.Roll = this.read();
+                            this.write("get /instrumentation/attitude-indicator/internal-pitch-deg\n");
+                            this.Pitch = this.read();
+                            this.write("get /instrumentation/altimeter/indicated-altitude-ft\n");
+                            this.Altimeter = this.read();
+                            this.write("get /instrumentation/heading-indicator/indicated-heading-deg\n");
+                            this.Heading = this.read();
+                            this.write("get /instrumentation/gps/indicated-ground-speed-kt\n");
+                            this.GroundSpeed = this.read();
+                            this.write("get /instrumentation/gps/indicated-vertical-speed\n");
+                            this.VerticalSpeed = this.read();
+                            this.write("get /position/latitude-deg\n");
+                            this.Latitude = this.read();
+                            this.write("get /position/longitude-deg\n");
+                            this.Longitude = this.read();
+                            //Write to server 
+                            while (this.queue.Count != 0)
+                            {
+                                this.write(queue.Dequeue());
+                                this.read();
+                            }
+                            Thread.Sleep(250);
+                        }
+                       
+                        catch
+                        {
+                            disconnected = true;
+                            connected = false;
+                            this.NotifyPropertyChanged("IsConnected");
+                            AutoClosingMessageBox.Show("Connection Interupted\nPlease Reconnect to Server\nClosing Window Please wait", "Error", 2500);
+                        }
+                    }
+                }).Start();            
+        }
+        //Set commands queue - to be write to Server in Thread
+        public void addToQueue(string msg)
+        {
+            this.queue.Enqueue(msg);
+        }
+
+        //ControlUnit- Joytick
+        public string Throttle
+        {
+            get
+            {
+                return this.throttle;
+            }
+            set
+            {
+                this.throttle = value;
+                string msg = "set /controls/engines/current-engine/throttle ";
+                msg += value.ToString();
+                msg += "\n";
+                this.addToQueue(msg);
+                this.NotifyPropertyChanged("Throttle");
+            }
+        }
+        public string Aileron
+        {
+            get
+            {
+                return this.aileron;
+            }
+            set
+            {
+                this.aileron = value;
+                string msg = "set /controls/flight/aileron ";
+                msg += value.ToString();
+                msg += "\n";
+                this.addToQueue(msg);
+                this.NotifyPropertyChanged("Aileron");
+
+            }
+        }
+        public string Elevator
+        {
+            get
+            {
+                return this.elevator;
+            }
+
+            set
+            {
+                this.elevator = value;
+                string msg = "set /controls/flight/elevator ";
+                msg += value.ToString();
+                msg += "\n";
+                this.addToQueue(msg);
+                this.NotifyPropertyChanged("Elevator");
+
+
+            }
+        }
+        public string Rudder
+        {
+            get
+            {
+                return this.rudder;
+            }
+            set
+            {
+                this.rudder = value;
+                string msg = "set /controls/flight/rudder ";
+                msg += value.ToString();
+                msg += "\n";
+                this.addToQueue(msg);
+                this.NotifyPropertyChanged("Rudder");
+
+            }
+        }
+        //Map 
+        public string Latitude
+        {
+            get { return this.latitude; }
+            set
+            {
+                this.latitude = value;
+                this.NotifyPropertyChanged("Location");
+                this.NotifyPropertyChanged("Latitude");
+            }
+        }
+        public string Longitude
+        {
+            get { return this.longitude; }
+            set
+            {
+
+                this.longitude = value;
+                this.NotifyPropertyChanged("Location");
+                this.NotifyPropertyChanged("Longitude");
+
+            }
+        }
+        public Location Location
+        {
+            get
+            {
+                double lat, lon;
+                try
+                {
+                    lat = double.Parse(this.latitude);
+                    lon = double.Parse(this.longitude);
+
+                }
+                catch
+                {
+                    return null;
+                }
+
+                return new Location(double.Parse(this.latitude), double.Parse(this.longitude));
+
+            }
+        }
+        //Dashboard 
+        public string Airspeed
+        {
+            get { return this.airspeed; }
+            set
+            {
+                this.airspeed = value;
+                this.NotifyPropertyChanged("Airspeed");
+            }
+        }
+        public string Altitude
+        {
+            get { return this.altitude; }
+            set
+            {
+                this.altitude = value;
+                this.NotifyPropertyChanged("Altitude");
+            }
+        }
+        public string Roll
+        {
+            get { return this.roll; }
+            set
+            {
+                this.roll = value;
+                this.NotifyPropertyChanged("Roll");
+            }
+        }
+        public string Pitch
+        {
+            get { return this.pitch; }
+            set
+            {
+                this.pitch = value;
+                this.NotifyPropertyChanged("Pitch");
+            }
+        }
+        public string Altimeter
+        {
+            get { return this.altimeter; }
+            set
+            {
+                this.altimeter = value;
+                this.NotifyPropertyChanged("Altimeter");
+            }
+        }
+        public string Heading
+        {
+            get { return this.heading; }
+            set
+            {
+                this.heading = value;
+                this.NotifyPropertyChanged("Heading");
+            }
+        }
+        public string GroundSpeed
+        {
+            get { return this.groundSpeed; }
+            set
+            {
+                this.groundSpeed = value;
+                this.NotifyPropertyChanged("GroundSpeed");
+            }
+        }
+        public string VerticalSpeed
+        {
+            get { return this.verticalSpeed; }
+            set
+            {
+                this.verticalSpeed = value;
+                this.NotifyPropertyChanged("VerticalSpeed");
+            }
+        }
+        //Connections
+        private bool Disconnected
+        {
+            get { return this.disconnected; }
+            set { this.disconnected = value; }
+        }
+        public bool Connected
+        {
+            get { return this.connected; }
+            set 
+            { 
+                this.connected = value;
+                this.NotifyPropertyChanged("IsConnected");
+
+            }
+        }
+        public string IsConnected
+        {
+            get 
+            {
+                if (Connected)
+                    return "Connected";
+                else
+                    return "Disconnected";
+
+            }
+        }
+
+    }
+}
